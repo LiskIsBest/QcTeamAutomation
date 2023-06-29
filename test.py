9031e2cc6bd4ba89143a3fc31b1e309aa1381adc
Darien Moore <Darienmoore2002@gmail.com>
2023-06-29 17:25:24 -0500

    working terminal version

--- test.py
+++ test.py
@@ -12,88 +12,89 @@ from selenium.webdriver.common.by import By
 email = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
 JAMF_URL = 'https://login.jamfschool.com/login'
-IPAD_ID = 'c121-33491'
+# IPAD_ID = 'c121-33491'
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 driver: webdriver.Chrome = webdriver.Chrome(options=chrome_options)
 driver.implicitly_wait(35)
-print(driver.window_handles[0])
 
 driver.get(JAMF_URL)
 driver_window_value: str = driver.current_window_handle
-# time.sleep(5)
 print('clicking email form input')
 jamf_email = driver.find_element(by=By.ID,value="email")
 jamf_email.send_keys(email)
-# time.sleep(0.2)
 
 print('clicking password form input')
 jamf_password = driver.find_element(by=By.ID, value="password")
 jamf_password.send_keys(password)
-# time.sleep(0.2)
 
 print('clicking sign in')
 sign_in = driver.find_element(by=By.XPATH, value='//*[@id="app"]/div/div/article/div/div/form/div[3]/button')
 sign_in.click()
-# time.sleep(5)
 
 print('opening devices drop menu')
 side_menu = driver.find_element(by=By.XPATH, value='/html/body/div[2]/aside/section/ul/li[2]/a')
 side_menu.click()
-# time.sleep(1)
 
 print('clicking inventory')
 inventory_option = driver.find_element(by=By.XPATH, value='//*[@id="devices"]/li[1]/a')
 inventory_option.click()
-# time.sleep(20)
 
-print("searching ipad")
-inventory_search = driver.find_element(by=By.CSS_SELECTOR, value='input[placeholder="Search..."]')
-inventory_search.send_keys(IPAD_ID)
-time.sleep(1.5)
+print()
+school = input("Enter school name: ")
+print('----------\n\n\n----------')
 
-print("clicking ipad")
-# link__2p2iH
-first_ipad = driver.find_element(by=By.CLASS_NAME, value='link__2p2iH')
-first_ipad.click()
-# time.sleep(3)
+while(True):
+    print('Ready to scan...')
+    scan = input("Scan serial number or asset tag (0 to quit): ")
+    if scan == '0':
+        driver.close()
+        driver.quit()
+        break
 
-print("clicking edit details")
-edit_details = driver.find_element(by=By.XPATH, value='//*[@id="details"]/div/div[1]/div/a')
-edit_details.click()
-# time.sleep(3)
+    print("searching ipad")
+    inventory_search = driver.find_element(by=By.CSS_SELECTOR, value='input[placeholder="Search..."]')
+    inventory_search.clear()
+    inventory_search.send_keys(scan)
+    time.sleep(1.4)
 
-print("change location button")
-change_button = driver.find_element(by=By.XPATH, value='//*[@id="toggle-move-to-location"]')
-change_button.click()
-# time.sleep(3)
+    print("clicking ipad")
+    first_ipad = driver.find_element(by=By.CLASS_NAME, value='link__2p2iH')
+    first_ipad.click()
 
-print("turning on force now")
-driver.execute_script('document.getElementById("force-move-option-checkbox").click()')
-# time.sleep(2)
+    print("clicking edit details")
+    edit_details = driver.find_element(by=By.XPATH, value='//*[@id="details"]/div/div[1]/div/a')
+    edit_details.click()
 
-print("selecting dawson elementary")
-school_drop_down = Select(driver.find_element(by=By.ID, value='select-new-location'))
-school_drop_down.select_by_visible_text(text="Dawson Elementary")
-# time.sleep(1.5)
+    print("change location button")
+    change_button = driver.find_element(by=By.XPATH, value='//*[@id="toggle-move-to-location"]')
+    change_button.click()
 
-print('saving details')
-save_details = driver.find_element(by=By.XPATH, value='//*[@id="modal-move-to-location-submit"]')
-save_details.click()
-# time.sleep(3)
+    print("turning on force now")
+    driver.execute_script('document.getElementById("force-move-option-checkbox").click()')
 
-print('saving ipad')
-save_ipad = driver.find_element(by=By.XPATH, value='//*[@id="btn-edit"]')
-save_ipad.click()
-# time.sleep(6)
+    print(f"selecting {school}")
+    school_drop_down = Select(driver.find_element(by=By.ID, value='select-new-location'))
+    time.sleep(1.4)
+    school_drop_down.select_by_visible_text(text=school)
 
-print('opening devices drop menu')
-side_menu = driver.find_element(by=By.XPATH, value='/html/body/div[2]/aside/section/ul/li[2]/a')
-side_menu.click()
-# time.sleep(1)
+    print('saving details')
+    save_details = driver.find_element(by=By.XPATH, value='//*[@id="modal-move-to-location-submit"]')
+    save_details.click()
 
-print('clicking inventory')
-inventory_option = driver.find_element(by=By.XPATH, value='//*[@id="devices"]/li[1]/a')
-inventory_option.click()
-# time.sleep(20)
+    print('saving ipad')
+    save_ipad = driver.find_element(by=By.XPATH, value='//*[@id="btn-edit"]')
+    save_ipad.click()
+
+    print('opening devices drop menu')
+    side_menu = driver.find_element(by=By.XPATH, value='/html/body/div[2]/aside/section/ul/li[2]/a')
+    side_menu.click()
+
+    print('clicking inventory')
+    inventory_option = driver.find_element(by=By.XPATH, value='//*[@id="devices"]/li[1]/a')
+    inventory_option.click()
+    time.sleep(2)
+
+    inventory_search = driver.find_element(by=By.CSS_SELECTOR, value='input[placeholder="Search..."]')
+    inventory_search.clear()
