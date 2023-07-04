ffc63b5c111f5ac6528ae2be6ca0583c17354b76
Darien Moore <Darienmoore2002@gmail.com>
2023-07-04 02:07:10 -0500

    update

--- gui.py
+++ gui.py
@@ -1,6 +1,15 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QLineEdit, QWidget, QPushButton, QLabel
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QVBoxLayout,
+    QHBoxLayout,
+    QLineEdit,
+    QWidget,
+    QPushButton,
+    QLabel,
+)
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -10,61 +19,64 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.email = '' 
-        self.password = ''
-
+        self.jamf_email = ''
+        self.jamf_password = ''
 
         self.items_scanned = []
 
         # window settings
-        self.setWindowTitle("Jamf auto thingy")
+        self.setWindowTitle('Jamf auto thingy')
         self.setFixedWidth(300)
         self.setFixedHeight(200)
-        
+
+        #---------------------------------------------------------------------#
+
         # debug labels
-        self.error_label = QLabel(text="")
-        self.driver_open_label = QLabel(parent=self, text="Chrome not open")
+        self.error_label = QLabel(text='')
+        self.driver_open_label = QLabel(parent=self, text='Chrome not open')
 
         # email input form
         self.email_input = QLineEdit(self)
-        self.email_input.setPlaceholderText("Email")
+        self.email_input.setPlaceholderText('Email')
         self.email_input.textChanged.connect(self.update_email)
 
         # password input form
         self.pass_input = QLineEdit(self)
         self.pass_input.setEchoMode(QLineEdit.EchoMode.Password)
-        self.pass_input.setPlaceholderText("Password")
+        self.pass_input.setPlaceholderText('Password')
         self.pass_input.textChanged.connect(self.update_password)
 
-        #login button
-        self.login_button = QPushButton(parent=self,text='Login')
+        # login button
+        self.login_button = QPushButton(parent=self, text='Login')
         self.login_button.clicked.connect(self.login_pressed)
-
-        # scanner item add button
-        self.scanner_add_button = QPushButton(parent=self, text="Add")
-        self.scanner_add_button.clicked.connect(self.scan_enter)
-
-        # scan items clear button
-        self.scanned_items_clear_button = QPushButton(parent=self, text="Clear")
-        self.scanned_items_clear_button.clicked.connect(self.scan_clear)
-
-        # scanner input form
-        self.scanner_input = QLineEdit(self)
-        self.scanner_input.setPlaceholderText("Scan")
-        self.scanner_input.returnPressed.connect(self.scanner_add_button.click)
-
-        # school input form
-        self.school_input = QLineEdit(self)
-        self.school_input.setPlaceholderText("School/Location")
 
         # layout section for login
         login_layout = QVBoxLayout()
-        login_layout.setContentsMargins(0,0,0,0)
+        login_layout.setContentsMargins(0, 0, 0, 0)
         login_layout.addWidget(self.email_input)
         login_layout.addWidget(self.pass_input)
         login_layout.addWidget(self.login_button)
         login_layout.addWidget(self.error_label)
         login_layout.addWidget(self.driver_open_label)
+
+        #---------------------------------------------------------------------#
+
+        # scanner input form
+        self.scanner_input = QLineEdit(self)
+        self.scanner_input.setPlaceholderText('Scan')
+        self.scanner_input.returnPressed.connect(self.scanner_add_button.click)
+
+        # school input form
+        self.school_input = QLineEdit(self)
+        self.school_input.setPlaceholderText('School/Location')
+
+        # scanner item add button
+        self.scanner_add_button = QPushButton(parent=self, text='Add')
+        self.scanner_add_button.clicked.connect(self.scan_enter)
+
+        # scan items clear button
+        self.scanned_items_clear_button = QPushButton(parent=self, text='Clear')
+        self.scanned_items_clear_button.clicked.connect(self.scan_clear)
 
         # layout section for scanning inputs
         scanner_layout = QVBoxLayout()
@@ -73,13 +85,16 @@ class MainWindow(QMainWindow):
         scanner_layout.addWidget(self.scanner_add_button)
         scanner_layout.addWidget(self.scanned_items_clear_button)
 
-        outerLayout = QHBoxLayout()
-        outerLayout.addLayout(login_layout)
-        outerLayout.addLayout(scanner_layout)
+        #---------------------------------------------------------------------#
+
+        # main layout
+        main_layout = QHBoxLayout()
+        main_layout.addLayout(login_layout)
+        main_layout.addLayout(scanner_layout)
 
         wid = QWidget()
         self.setCentralWidget(wid)
-        wid.setLayout(outerLayout)
+        wid.setLayout(main_layout)
 
     def update_email(self, s):
         self.email = s
@@ -88,25 +103,29 @@ class MainWindow(QMainWindow):
         self.password = s
 
     def login_pressed(self):
-        match(self.email, self.password):
-            case '', '':
+        match (self.jamf_email, self.jamf_password):
+            case "", "":
                 self.error_label.setText('No email or password entered!')
                 self.error_label.adjustSize()
                 return
-            case _, '':
+            case _, "":
                 self.error_label.setText('No password entered!')
                 self.error_label.adjustSize()
                 return
-            case '', _:
+            case "", _:
                 self.error_label.setText('No email entered!')
                 self.error_label.adjustSize()
                 return
+        self.error_label.setText('Logging in.')
         chrome_options = Options()
-        chrome_options.add_experimental_option("detach", True)
+        chrome_options.add_experimental_option('detach', True)
         self.chrome_driver: webdriver.Chrome = webdriver.Chrome(options=chrome_options)
         self.chrome_driver.implicitly_wait(35)
         self.driver_open_label.setText('Chrome open')
-        jamfs.open_jamf_site(driver=self.chrome_driver, email=self.email, password=self.password)
+        jamfs.open_jamf_site(
+            driver=self.chrome_driver, email=self.jamf_email, password=self.jamf_password
+        )
+        self.error_label.setText('')
 
     def scan_enter(self):
         item = self.scanner_input.text()
@@ -114,11 +133,12 @@ class MainWindow(QMainWindow):
         jamfs.update_jamf(driver=self.chrome_driver, SNorASSET=item, school=school)
         # self.items_scanned.append(item)
         # print(f"adding {item} to list:{self.items_scanned}")
-        self.scanner_input.setText('')
+        self.scanner_input.setText("")
 
     def scan_clear(self):
         self.items_scanned = []
         print(f"cleared list:{self.items_scanned}")
+
 
 app = QApplication(sys.argv)
 
--- jamfs.py
+++ jamfs.py
@@ -3,42 +3,66 @@ from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
 
+def gen_element(by, value)->dict:
+    return {
+        'by': by,
+        'value': value
+    }
+    
+
 JAMF_URL = 'https://login.jamfschool.com/login'
+
+JAMF_EMAIL = gen_element(by=By.ID, value='email')
+JAMF_PASSWORD = gen_element(by=By.ID, value='password')
+JAMF_SIGN_IN_BUTTON = gen_element(by=By.XPATH, value='//*[@id="app"]/div/div/article/div/div/form/div[3]/button')
+DEVICES_SIDE_MENU = gen_element(by=By.XPATH, value='/html/body/div[2]/aside/section/ul/li[2]/a')
+INVENTORY_OPTION = gen_element(by=By.XPATH, value='//*[@id="devices"]/li[1]/a')
+INVENTORY_SEARCH = gen_element(by=By.CSS_SELECTOR, value='input[placeholder="Search..."]')
+FIRST_IPAD_RESULT = gen_element(by=By.XPATH, value='//*[@id="app"]/main/div/div[2]/div/div/table/tbody/tr/td[2]/div/div[2]/a')
+EDIT_DETAILS_BUTTON = gen_element(by=By.XPATH, value='//*[@id="details"]/div/div[1]/div/a')
+LOCATION_CHANGE_BUTTON = gen_element(by=By.ID, value='toggle-move-to-location')
+SCHOOL_LIST_DROP_DOWN = gen_element(by=By.ID, value='select-new-location')
+SAVE_DETAILS_BUTTON = gen_element(by=By.ID, value='modal-move-to-location-submit')
+SAVE_IPAD_BUTTON = gen_element(by=By.ID, value='btn-edit')
+
+FORCE_MOVE_JAVASCRIPT = 'document.getElementById("force-move-option-checkbox").click()'
 
 
 def open_jamf_site(driver: webdriver.Chrome, email: str, password: str) -> str:
     driver.get(JAMF_URL)
     driver_window_value: str = driver.current_window_handle
-    jamf_email = driver.find_element(by="id",value="email")
+    jamf_email = driver.find_element(**JAMF_EMAIL)
     jamf_email.send_keys(email)
-    jamf_password = driver.find_element(by="id", value="password")
+    jamf_password = driver.find_element(**JAMF_PASSWORD)
     jamf_password.send_keys(password)
-    sign_in = driver.find_element(by='xpath', value='//*[@id="app"]/div/div/article/div/div/form/div[3]/button')
+    sign_in = driver.find_element(**JAMF_SIGN_IN_BUTTON)
     sign_in.click()
-    side_menu = driver.find_element(by=By.XPATH, value='/html/body/div[2]/aside/section/ul/li[2]/a')
+    side_menu = driver.find_element(**DEVICES_SIDE_MENU)
     side_menu.click()
-    inventory_option = driver.find_element(by=By.XPATH, value='//*[@id="devices"]/li[1]/a')
+    inventory_option = driver.find_element(**INVENTORY_OPTION)
     inventory_option.click()
     return driver_window_value
 
+
 def update_jamf(driver: webdriver.Chrome, SNorASSET: str, school: str) -> None:
-    inventory_search = driver.find_element(by=By.CSS_SELECTOR, value='input[placeholder="Search..."]')
+    inventory_search = driver.find_element(**INVENTORY_SEARCH)
     inventory_search.send_keys(SNorASSET)
     time.sleep(1.5)
-    first_ipad = driver.find_element(by=By.XPATH, value='//*[@id="app"]/main/div/div[2]/div/div/table/tbody/tr/td[2]/div/div[2]/a')
+    first_ipad = driver.find_element(**FIRST_IPAD_RESULT)
     first_ipad.click()
-    edit_details = driver.find_element(by=By.XPATH, value='//*[@id="details"]/div/div[1]/div/a')
+    edit_details = driver.find_element(**EDIT_DETAILS_BUTTON)
     edit_details.click()
-    change_button = driver.find_element(by=By.XPATH, value='//*[@id="toggle-move-to-location"]')
+    change_button = driver.find_element(**LOCATION_CHANGE_BUTTON)
     change_button.click()
-    driver.execute_script('document.getElementById("force-move-option-checkbox").click()')
-    school_drop_down = Select(driver.find_element(by=By.ID, value='select-new-location'))
+    driver.execute_script(FORCE_MOVE_JAVASCRIPT)
+    school_drop_down = Select(driver.find_element(**SCHOOL_LIST_DROP_DOWN))
+    time.sleep(1.4)
     school_drop_down.select_by_visible_text(text=school)
-    save_details = driver.find_element(by=By.XPATH, value='//*[@id="modal-move-to-location-submit"]')
+    save_details = driver.find_element(**SAVE_DETAILS_BUTTON)
     save_details.click()
-    save_ipad = driver.find_element(by=By.XPATH, value='//*[@id="btn-edit"]')
+    save_ipad = driver.find_element(**SAVE_IPAD_BUTTON)
     save_ipad.click()
-    side_menu = driver.find_element(by=By.XPATH, value='/html/body/div[2]/aside/section/ul/li[2]/a')
+    side_menu = driver.find_element(**DEVICES_SIDE_MENU)
     side_menu.click()
-    inventory_option = driver.find_element(by=By.XPATH, value='//*[@id="devices"]/li[1]/a')
+    inventory_option = driver.find_element(**INVENTORY_OPTION)
     inventory_option.click()
