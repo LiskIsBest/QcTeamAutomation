23f438b9f577d6499df6179de7f542c724b34741
Darien Moore <Darienmoore2002@gmail.com>
2023-07-04 06:38:44 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -9,6 +9,8 @@ from PyQt5.QtWidgets import (
     QWidget,
     QPushButton,
     QLabel,
+    QComboBox,
+    QCheckBox,
 )
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -22,17 +24,20 @@ class MainWindow(QMainWindow):
         self.jamf_email = ''
         self.jamf_password = ''
 
+        self.jamf_selected_school = ''
+
         self.items_scanned = []
 
         # window settings
         self.setWindowTitle('Jamf auto thingy')
-        self.setFixedWidth(300)
-        self.setFixedHeight(200)
-
+        self.setFixedWidth(800)
+        self.setFixedHeight(350)
+        self.setStyleSheet("border: 1px solid; border-color:red;") #uncomment to show border boxes
         #---------------------------------------------------------------------#
 
         # debug labels
         self.error_label = QLabel(text='')
+        self.error_label.resize(350,15)
         self.driver_open_label = QLabel(parent=self, text='Chrome not open')
 
         # email input form
@@ -58,17 +63,12 @@ class MainWindow(QMainWindow):
         login_layout.addWidget(self.login_button)
         login_layout.addWidget(self.error_label)
         login_layout.addWidget(self.driver_open_label)
+        login_widget = QWidget()
+        login_widget.setLayout(login_layout)
+        login_widget.setFixedWidth(250)
 
         #---------------------------------------------------------------------#
 
-        # scanner input form
-        self.scanner_input = QLineEdit(self)
-        self.scanner_input.setPlaceholderText('Scan')
-        self.scanner_input.returnPressed.connect(self.scanner_add_button.click)
-
-        # school input form
-        self.school_input = QLineEdit(self)
-        self.school_input.setPlaceholderText('School/Location')
 
         # scanner item add button
         self.scanner_add_button = QPushButton(parent=self, text='Add')
@@ -78,41 +78,68 @@ class MainWindow(QMainWindow):
         self.scanned_items_clear_button = QPushButton(parent=self, text='Clear')
         self.scanned_items_clear_button.clicked.connect(self.scan_clear)
 
+        # scanner input form
+        self.scanner_input = QLineEdit(self)
+        self.scanner_input.setPlaceholderText('Scan')
+        self.scanner_input.returnPressed.connect(self.scanner_add_button.click)
+
+        # school drop down
+        self.school_selection = QComboBox(self)
+        self.school_selection.addItems(jamfs.JAMF_SCHOOLS)
+        self.school_selection.currentTextChanged.connect(self.updated_selected_school)
+        
+        # school drop down lock button
+        self.school_selection_lock = QCheckBox(self)
+        self.school_selection_lock.setText('Lock school')
+        self.school_selection_lock.stateChanged.connect(self.lock_selected_school)
+
         # layout section for scanning inputs
         scanner_layout = QVBoxLayout()
-        scanner_layout.addWidget(self.school_input)
+        scanner_layout.setContentsMargins(0, 0, 0, 0)
+        scanner_layout.addWidget(self.school_selection)
+        scanner_layout.addWidget(self.school_selection_lock)
         scanner_layout.addWidget(self.scanner_input)
         scanner_layout.addWidget(self.scanner_add_button)
         scanner_layout.addWidget(self.scanned_items_clear_button)
+        scanner_widget = QWidget()
+        scanner_widget.setLayout(scanner_layout)
+        scanner_widget.setFixedWidth(400)
 
         #---------------------------------------------------------------------#
 
         # main layout
         main_layout = QHBoxLayout()
-        main_layout.addLayout(login_layout)
-        main_layout.addLayout(scanner_layout)
+        main_layout.addWidget(login_widget)
+        main_layout.addWidget(scanner_widget)
 
         wid = QWidget()
         self.setCentralWidget(wid)
         wid.setLayout(main_layout)
 
     def update_email(self, s):
-        self.email = s
+        self.jamf_email = s
 
     def update_password(self, s):
-        self.password = s
+        self.jamf_password = s
+
+    def updated_selected_school(self, s):
+        self.jamf_selected_school = s
+        print(self.jamf_selected_school)
+
+    def lock_selected_school(self):
+        self.school_selection.setEnabled(not self.school_selection.isEnabled())
 
     def login_pressed(self):
         match (self.jamf_email, self.jamf_password):
-            case "", "":
+            case '', '':
                 self.error_label.setText('No email or password entered!')
                 self.error_label.adjustSize()
                 return
-            case _, "":
+            case _, '':
                 self.error_label.setText('No password entered!')
                 self.error_label.adjustSize()
                 return
-            case "", _:
+            case '', _:
                 self.error_label.setText('No email entered!')
                 self.error_label.adjustSize()
                 return
