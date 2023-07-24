fc03d60d0133175a42e5b1486e885ce17dca4f57
Darien Moore <LiskIsBest@gmail.com>
2023-07-24 13:16:52 -0500

    replaced code with rewrite

--- python-impl/auto_jamf.py
+++ python-impl/auto_jamf.py
@@ -1,13 +1,14 @@
 import time
 import os
+from enum import Enum
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.edge.options import Options as EdgeOptions
-from selenium.webdriver.firefox.options import Options as FireOptions
-from selenium.webdriver.firefox.service import Service as FireService
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.common.exceptions import (
     NoSuchWindowException,
     SessionNotCreatedException,
@@ -20,10 +21,6 @@ from selenium.common.exceptions import (
 from urllib3.exceptions import ProtocolError
 import maskpass
 
-JAMF_URL: str = "https://login.jamfschool.com/login"
-FORCE_MOVE_JAVASCRIPT: str = (
-    'document.getElementById("force-move-option-checkbox").click()'
-)
 
 # ANSI escape characters for colors
 # Border color "Yellow"
@@ -35,242 +32,232 @@ WC = "\033[0;31m"
 # Color terminator
 CT = "\033[00m"
 
-def main() -> None:
-    JAMF_EMAIL: dict = element_dict(by=By.ID, value="email")
-    JAMF_PASSWORD: dict = element_dict(by=By.ID, value="password")
-    JAMF_SIGN_IN_BUTTON: dict = element_dict(
-        by=By.XPATH, value='//*[@id="app"]/div/div/article/div/div/form/div[3]/button'
-    )
-    JAMF_SIGN_IN_ERROR: dict = element_dict(
-        by=By.CLASS_NAME,
-        value="error-text__29lYK",
-    )
-    JAMF_AUTH_FORM: dict = element_dict(
-        by=By.XPATH, value='//*[@id="app"]/div/div/article/div/form/div[1]/div[2]/input'
-    )
+# ANSI escape characters to move terminal cursor
+LINE_UP: str = "\033[1F"  # also moves cursor to beginning of line
+LINE_DOWN: str = "\033[1B"
+LINE_CLEAR: str = "\033[2K"
+SEPERATOR: str = "-" * 38
 
-    DEVICES_SIDE_MENU: dict = element_dict(
-        by=By.XPATH, value='//*[@id="app"]/aside/nav[1]/ul/li[2]/a'
-    )
-    DEVICES_SIDE_MENU2: dict = element_dict(
-        by=By.CSS_SELECTOR, value='a[data-target="#devices"]'
-    )
-    INVENTORY_SEARCH: dict = element_dict(
-        by=By.CSS_SELECTOR, value='input[placeholder="Search..."]'
-    )
 
-    FIRST_IPAD_RESULT: dict = element_dict(
-        by=By.XPATH,
-        value='//*[@id="app"]/main/div/div[2]/div/div/table/tbody/tr/td[2]/div/div[2]/a',
-    )
-    EDIT_DETAILS_BUTTON: dict = element_dict(
-        by=By.XPATH, value='//*[@id="details"]/div/div[1]/div/a'
-    )
-    USER_BOX: dict = element_dict(by=By.XPATH, value='//*[@id="owner-id"]/option')
-    CURRENT_USER: dict = element_dict(
-        by=By.CLASS_NAME, value="select2-selection__clear"
-    )
+class Browsers(Enum):
+    CHROME = "1"
+    EDGE = "2"
+    FIREFOX = "3"
 
-    ERASE_IPAD_BUTTON: dict = element_dict(
-        by=By.XPATH, value='//*[@id="importantButton"]'
-    )
-    ERASE_IPAD_CONFIRM: dict = element_dict(by=By.ID, value="wipe-submit")
 
-    LOCATION_CHANGE_BUTTON: dict = element_dict(
-        by=By.ID, value="toggle-move-to-location"
-    )
-    SCHOOL_LIST_DROP_DOWN: dict = element_dict(by=By.ID, value="select-new-location")
-    SAVE_DETAILS_BUTTON: dict = element_dict(
-        by=By.ID, value="modal-move-to-location-submit"
-    )
-    SAVE_IPAD_BUTTON: dict = element_dict(by=By.ID, value="btn-edit")
+class AutoJamf:
+    chromeOptions = ChromeOptions()
+    chromeOptions.add_experimental_option("excludeSwitches", ["enable-logging"])
+    edgeOptions = EdgeOptions()
+    edgeOptions.add_experimental_option("excludeSwitches", ["enable-logging"])
+    service = Service(log_path=os.path.devnull)  # for firefox
+    default_wait = 30
 
-    print(
-        f"""
+    title_card = f"""
     {BC}=============================={CT}
-    {BC}|{CT}      {TC}Jamf auto update-{CT}     {BC}|{CT}
-    {BC}|{CT}       {TC}I-pad location{CT}       {BC}|{CT}
+    {BC}|{CT}  {TC}Jamf process automation{CT}   {BC}|{CT}
     {BC}|{CT}                            {BC}|{CT}
     {BC}|{CT}  Created by: Darien Moore  {BC}|{CT}
     {BC}|{CT}============================{BC}|{CT}
     {BC}|{CT}   press Ctrl + C to exit.  {BC}|{CT}
     {BC}=============================={CT}"""
-    )
 
-    print(f"{SEPERATOR}\n")
-    school: str = footer_input("Enter the location name (check jamf for exact name): ")
-    email: str = footer_input("Enter Jamf username/email: ")
-    password: str = footer_mask(prompt="Enter your Jamf password: ")
-    wipe: str = footer_input("Turn on the wipe functionality? (default yes) y/n: ")
-    browser: str = footer_input(
-        "Enter your desired browser (default: Chrome)\n 1. Chrome/Chromium, 2. Edge, 3. Firefox: ",
-        depth=2,
-    )
-    minimized: str = footer_input("Start browser minimized? (default yes) y/n: ")
-    if browser not in {"1", "2", "3"}:
-        browser = "1"
-
-    clear_lines(7)
-
-    driver: webdriver.Chrome | webdriver.Edge | webdriver.Firefox
-    match (browser):
-        case "1":
-            options = ChromeOptions()
-            options.add_experimental_option("excludeSwitches", ["enable-logging"])
-            driver = webdriver.Chrome(options=options)
-        case "2":
-            options = EdgeOptions()
-            options.add_experimental_option("excludeSwitches", ["enable-logging"])
-            driver = webdriver.Edge(options=options)
-        case "3":
-            options = FireOptions()
-            service = FireService(log_path=os.path.devnull)
-            driver = webdriver.Firefox(options=options, service=service)
-
-    driver_handle = driver.current_window_handle
-
-    if minimized.lower() != "n":
-        driver.minimize_window()
-
-    driver.implicitly_wait(35)
-
-    driver.get(JAMF_URL)
-
-    lines_to_clear: int = 2
-
-    footer_print("Logging in")
-    driver.find_element(**JAMF_EMAIL).send_keys(email)
-    driver.find_element(**JAMF_PASSWORD).send_keys(password)
-    driver.find_element(**JAMF_SIGN_IN_BUTTON).click()
-
-    # Sign in failed check
-    try:
-        driver.implicitly_wait(5)
-        driver.find_element(**JAMF_SIGN_IN_ERROR)
-        raise SignInFailed
-    except (NoSuchElementException, ElementNotVisibleException):
-        pass
-
-    # Authenticator code check
-    try:
-        driver.implicitly_wait(5)
-        driver.find_element(**JAMF_AUTH_FORM)
-        if isinstance(driver, webdriver.Firefox):
-            driver.maximize_window()
-        else:
-            driver.switch_to.window(driver_handle)
-        lines_to_clear += 2
-        driver.implicitly_wait(60)
-        footer_print(f"{TC}You have 60 seconds enter the authenticator code.{CT}")
-        footer_print(
-            f"{TC}Program will close if authenticator code is not entererd.{CT}"
+    def __init__(self) -> None:
+        print(f"{self.title_card}\n{SEPERATOR}\n")
+        self.school: str = footer_input(
+            "Enter the location name (check jamf for exact name): "
+        )
+        self.email: str = footer_input("Enter Jamf username/email: ")
+        self.password: str = footer_mask(prompt="Enter your Jamf password: ")
+        self.wipe: bool = (
+            False
+            if footer_input("Turn on the wipe functionality? (default Yes) Y/n: ")
+            == "N".lower()
+            else True
+        )
+        self.minimize: bool = (
+            True
+            if footer_input("Start browser minimized? (default No) y/N: ")
+            == "Y".lower()
+            else False
         )
         try:
-            driver.find_element(**DEVICES_SIDE_MENU)
-        except TimeoutException:
-            raise NoAuthCode
-        driver.implicitly_wait(35)
-        if minimized != "n".lower():
+            self.browser: Browsers = Browsers(
+                footer_input(
+                    "Enter your desired browser (default: Chrome)\n 1. Chrome/Chromium, 2. Edge, 3. Firefox: ",
+                    depth=2,
+                )
+            )
+        except:
+            self.browser = Browsers.CHROME
+
+        clear_lines(7)
+
+        self.driver = self.open_browser(browser=self.browser)
+        self.driver_handle = self.driver.current_window_handle
+
+        clear_lines(self.login(email=self.email, password=self.password))
+
+        while True:
+            self.driver.implicitly_wait(self.default_wait)
+            footer_print("Loading inventory")
+            self.driver.find_element(**INVENTORY_SEARCH)
+            clear_lines(1)
+
+            scan = footer_input(" Scan serial number or asset tag (0 to quit): ")
+            if scan == "0" or scan == "":
+                self.driver.close()
+                self.driver.quit()
+                break
+
+            lines_to_clear = self.process(scan=scan)
+            clear_lines(lines_to_clear)
+
+    def open_browser(
+        self, browser: Browsers
+    ) -> webdriver.Chrome | webdriver.Edge | webdriver.Firefox:
+        match (browser):
+            case Browsers.CHROME:
+                driver = webdriver.Chrome(options=self.chromeOptions)
+            case Browsers.EDGE:
+                driver = webdriver.Edge(options=self.edgeOptions)
+            case Browsers.FIREFOX:
+                driver = webdriver.Firefox(service=self.service)
+        driver.implicitly_wait(self.default_wait)
+
+        if self.minimize:
             driver.minimize_window()
-    except (NoSuchElementException, ElementNotVisibleException):
-        pass
 
-    footer_print('Opening "Devices/Inventory" menu')
-    driver.get("https://austinisd.jamfcloud.com/devices")
+        return driver
 
-    clear_lines(lines_to_clear)
+    def login(self, email: str, password: str) -> int:
+        self.driver.get(JAMF_URL)
 
-    driver.implicitly_wait(35)
-    while True:
-        lines_to_clear = 11
-        footer_print("Loading inventory")
-        driver.find_element(**DEVICES_SIDE_MENU)
-        inventory_search = driver.find_element(**INVENTORY_SEARCH)
-        clear_lines(1)
+        lines_to_clear: int = 2
 
-        scan = footer_input(" Scan serial number or asset tag (0 to quit): ")
-        if scan == "0" or scan == "":
-            driver.close()
-            driver.quit()
-            break
+        footer_print("Logging in")
+        self.driver.find_element(**JAMF_EMAIL).send_keys(email)
+        self.driver.find_element(**JAMF_PASSWORD).send_keys(password)
+        self.driver.find_element(**JAMF_SIGN_IN_BUTTON).click()
+
+        # Sign in failed check
+        try:
+            self.driver.implicitly_wait(5)
+            self.driver.find_element(**JAMF_SIGN_IN_ERROR)
+            raise SignInFailed
+        except (NoSuchElementException, ElementNotVisibleException):
+            pass
+
+        # Authenticator code check
+        try:
+            self.driver.implicitly_wait(5)
+            self.driver.find_element(**JAMF_AUTH_FORM)
+            if isinstance(self.driver, webdriver.Firefox):
+                self.driver.maximize_window()
+            else:
+                self.driver.switch_to.window(self.driver_handle)
+            lines_to_clear += 2
+            self.driver.implicitly_wait(60)
+            footer_print(f"{TC}You have 60 seconds enter the authenticator code.{CT}")
+            footer_print(
+                f"{TC}Program will close if authenticator code is not entererd.{CT}"
+            )
+            try:
+                self.driver.find_element(**DEVICES_SIDE_MENU)
+            except TimeoutException:
+                raise NoAuthCode
+            self.driver.implicitly_wait(self.default_wait)
+            if self.minimize:
+                self.driver.minimize_window()
+        except (NoSuchElementException, ElementNotVisibleException):
+            pass
+
+        footer_print('Opening "Devices/Inventory" menu')
+        self.driver.get("https://austinisd.jamfcloud.com/devices")
+
+        return lines_to_clear
+
+    def process(self, scan: str) -> int:
+        lines_to_clear: int = 10
 
         footer_print(f"Searching for I-pad:{scan}")
+        inventory_search: WebElement = self.driver.find_element(**INVENTORY_SEARCH)
         inventory_search.clear()
         inventory_search.send_keys(scan)
         time.sleep(1.1)
 
         try:
-            driver.implicitly_wait(2)
-            driver.find_element(**FIRST_IPAD_RESULT).click()
+            self.driver.implicitly_wait(2)
+            self.driver.find_element(**FIRST_IPAD_RESULT).click()
             footer_print(f"Found I-pad:{scan}, clicking")
         except NoSuchElementException:
-            footer_print(f'I-pad:{scan} not found. Returning to scan')
+            footer_print(f"I-pad:{scan} not found. Returning to scan")
             time.sleep(3)
-            clear_lines(3)
-            driver.implicitly_wait(35)
-            continue
+            return 3
 
-        driver.implicitly_wait(35)
-        footer_print('Opening "Edit details" menu')
-        driver.find_element(**EDIT_DETAILS_BUTTON).click()
-
-        footer_print('Clicking "Change" button')
-        driver.find_element(**LOCATION_CHANGE_BUTTON).click()
-
-        footer_print('Turning on "Force now"')
-        driver.execute_script(FORCE_MOVE_JAVASCRIPT)
-
-        footer_print(f"Selecting {school} from drop down")
-        school_drop_down = Select(driver.find_element(**SCHOOL_LIST_DROP_DOWN))
-        time.sleep(1.3)
-        school_drop_down.select_by_visible_text(text=school)
-
-        footer_print("Saving details")
-        driver.find_element(**SAVE_DETAILS_BUTTON).click()
-
-        if wipe.lower() != 'n':
+        self.location_change()
+        if self.wipe:
             lines_to_clear += 1
-            if driver.find_element(**USER_BOX).text == "":
-                footer_print("No user to clear")
-            else:
-                footer_print("Clearing user")
-                driver.find_element(**CURRENT_USER).click()
+            self.wipe_user()
 
         footer_print("Saving I-pad")
-        driver.find_element(**SAVE_IPAD_BUTTON).click()
+        self.driver.find_element(**SAVE_IPAD_BUTTON).click()
 
-        footer_print(f"Location change for I-pad:{scan} completed.")
-        time.sleep(0.4)
-
-        if wipe.lower() != 'n':
+        if self.wipe:
             lines_to_clear += 1
-            footer_print(f"Factory reseting I-pad:{scan}")
-            driver.find_element(**ERASE_IPAD_BUTTON).click()
-            driver.find_element(**ERASE_IPAD_CONFIRM).click()
+            self.erase_ipad(scan=scan)
 
         footer_print('Return to "Inventory" page')
-        driver.find_element(**DEVICES_SIDE_MENU2)
-        driver.get("https://austinisd.jamfcloud.com/devices")
+        self.driver.find_element(**DEVICES_SIDE_MENU2)
+        self.driver.get("https://austinisd.jamfcloud.com/devices")
 
-        clear_lines(lines_to_clear)
+        return lines_to_clear
+
+    def location_change(self) -> None:
+        footer_print('Opening "Edit details" menu')
+        self.driver.find_element(**EDIT_DETAILS_BUTTON).click()
+
+        footer_print('Clicking "Change" button')
+        self.driver.find_element(**LOCATION_CHANGE_BUTTON).click()
+
+        footer_print('Turning on "Force now"')
+        self.driver.execute_script(FORCE_MOVE_JAVASCRIPT)
+
+        footer_print(f"Selecting {self.school} from drop down")
+        school_drop_down = Select(self.driver.find_element(**SCHOOL_LIST_DROP_DOWN))
+        time.sleep(1.3)
+        school_drop_down.select_by_visible_text(text=self.school)
+
+        footer_print("Saving details")
+        self.driver.find_element(**SAVE_DETAILS_BUTTON).click()
+
+    def wipe_user(self) -> None:
+        if self.driver.find_element(**USER_BOX).text == "":
+            footer_print("No user to clear")
+        else:
+            footer_print("Clearing user")
+            self.driver.find_element(**CURRENT_USER).click()
+
+    def erase_ipad(self, scan: str) -> None:
+        footer_print(f"Factory reseting I-pad:{scan}")
+        self.driver.find_element(**ERASE_IPAD_BUTTON).click()
+        self.driver.find_element(**ERASE_IPAD_CONFIRM).click()
+
+    def shutdown(self) -> None:
+        pass
 
 
-# ANSI escape characters to move terminal cursor
-LINE_UP: str = "\033[1A"
-LINE_DOWN: str = "\033[1B"
-LINE_CLEAR: str = "\x1b[2K"
-SEPERATOR: str = "-" * 38
+class SignInFailed(Exception):
+    pass
+
+
+class NoAuthCode(Exception):
+    pass
 
 
 def element_dict(by: By | str = By.ID, value: str | None = None) -> dict:
     """generate dictionary with keyward args for "driver.find_element()"."""
     return {"by": by, "value": value}
-
-
-def clear_lines(amount: int) -> None:
-    """clear X lines above current terminal cursor location."""
-    for _ in range(amount):
-        print(LINE_UP, end=LINE_CLEAR)
 
 
 def footer_print(
@@ -308,17 +295,63 @@ def footer_mask(prompt: str) -> str:
     return val
 
 
-class SignInFailed(Exception):
-    pass
+def clear_lines(amount: int) -> None:
+    """clear X lines above current terminal cursor location."""
+    for _ in range(amount):
+        print(LINE_UP, end=LINE_CLEAR)
 
 
-class NoAuthCode(Exception):
-    pass
+JAMF_URL: str = "https://login.jamfschool.com/login"
+FORCE_MOVE_JAVASCRIPT: str = (
+    'document.getElementById("force-move-option-checkbox").click()'
+)
+JAMF_EMAIL: dict = element_dict(by=By.ID, value="email")
+JAMF_PASSWORD: dict = element_dict(by=By.ID, value="password")
+JAMF_SIGN_IN_BUTTON: dict = element_dict(
+    by=By.XPATH, value='//*[@id="app"]/div/div/article/div/div/form/div[3]/button'
+)
+JAMF_SIGN_IN_ERROR: dict = element_dict(
+    by=By.CLASS_NAME,
+    value="error-text__29lYK",
+)
+JAMF_AUTH_FORM: dict = element_dict(
+    by=By.XPATH, value='//*[@id="app"]/div/div/article/div/form/div[1]/div[2]/input'
+)
+
+DEVICES_SIDE_MENU: dict = element_dict(
+    by=By.XPATH, value='//*[@id="app"]/aside/nav[1]/ul/li[2]/a'
+)
+DEVICES_SIDE_MENU2: dict = element_dict(
+    by=By.CSS_SELECTOR, value='a[data-target="#devices"]'
+)
+INVENTORY_SEARCH: dict = element_dict(
+    by=By.CSS_SELECTOR, value='input[placeholder="Search..."]'
+)
+
+FIRST_IPAD_RESULT: dict = element_dict(
+    by=By.XPATH,
+    value='//*[@id="app"]/main/div/div[2]/div/div/table/tbody/tr/td[2]/div/div[2]/a',
+)
+EDIT_DETAILS_BUTTON: dict = element_dict(
+    by=By.XPATH, value='//*[@id="details"]/div/div[1]/div/a'
+)
+USER_BOX: dict = element_dict(by=By.XPATH, value='//*[@id="owner-id"]/option')
+CURRENT_USER: dict = element_dict(by=By.CLASS_NAME, value="select2-selection__clear")
+
+ERASE_IPAD_BUTTON: dict = element_dict(by=By.XPATH, value='//*[@id="importantButton"]')
+ERASE_IPAD_CONFIRM: dict = element_dict(by=By.ID, value="wipe-submit")
+
+LOCATION_CHANGE_BUTTON: dict = element_dict(by=By.ID, value="toggle-move-to-location")
+SCHOOL_LIST_DROP_DOWN: dict = element_dict(by=By.ID, value="select-new-location")
+SAVE_DETAILS_BUTTON: dict = element_dict(
+    by=By.ID, value="modal-move-to-location-submit"
+)
+SAVE_IPAD_BUTTON: dict = element_dict(by=By.ID, value="btn-edit")
 
 
-if __name__ == "__main__":
+def main():
     try:
-        main()
+        AutoJamf()
     except TimeoutException:
         print("\n")
         print(
@@ -351,3 +384,7 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\n")
         print(f"{TC}Ctrl+C pressed. Terminating program.{CT}")
+
+
+if __name__ == "__main__":
+    main()
